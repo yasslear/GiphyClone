d3066ee193ad0893fb58f208f384b68e1213b8eb
unknown <elaamriyass@gmail.com>
2024-09-16 10:45:19 +0100

    further changes to base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // base URL for root deployment
-})
+  base: '/GiphyClone/', // This should match your GitHub Pages repository name
+});
